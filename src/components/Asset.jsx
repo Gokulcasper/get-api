5a503a5ed91463d99fb901a12a5f845bd113fec6
Gokulcasper <ronitcasper215@gmail.com>
2022-08-03 11:13:36 +0530

    Feat : Fetch Method

--- src/components/Asset.jsx
+++ src/components/Asset.jsx
@@ -1,16 +1,17 @@
-import axios from "axios";
+// import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const Asset = () => {
   const [posts, setPosts] = useState([]);
 
   const getPost = () => {
-    // fetch("https://jsonplaceholder.typicode.com/posts")
-    //   .then((data) => data.json())
-    //   .then((data) => setPosts(data));
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => setPosts(res.data));
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((data) => data.json())
+      .then((data) => setPosts(data));
+    // Or
+    // axios
+    //   .get("https://jsonplaceholder.typicode.com/posts")
+    //   .then((res) => setPosts(res.data));
   };
 
   useEffect(() => {
